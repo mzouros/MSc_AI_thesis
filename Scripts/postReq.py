ca1d6b6b787149580b16e63bf1b021d579deaed1
Michael Zouros <41365637+mzouros@users.noreply.github.com>
2021-12-26 17:05:19 +0200

    Updated to /predict on all tasks (API v1.1)

--- Scripts/postReq.py
+++ Scripts/postReq.py
@@ -1,5 +1,20 @@
+import sys
+import os
 import requests
 import json
 
-resp = requests.post("http://photostyleanalysis.ddns.net/psa-controller/predict", headers={'X-API-KEY':'**********'}, files = {"file": open('/path/to/image', 'rb')})
-print(resp.json())
+api = 'http://194.177.192.229:5000/psa-controller/predict/'
+psswd = '********'
+imagePath = input("Please enter the path of the image. Acceptable extensions: ['png', 'jpg', 'jpeg']: ")
+
+color = requests.post(api+'color', headers={'X-API-KEY':psswd}, files = {"file": open(imagePath, 'rb')})
+composition = requests.post(api+'composition', headers={'X-API-KEY':psswd}, files = {"file": open(imagePath, 'rb')})
+depth = requests.post(api+'depth', headers={'X-API-KEY':psswd}, files = {"file": open(imagePath, 'rb')})
+palette = requests.post(api+'palette', headers={'X-API-KEY':psswd}, files = {"file": open(imagePath, 'rb')})
+typee = requests.post(api+'type', headers={'X-API-KEY':psswd}, files = {"file": open(imagePath, 'rb')})
+
+print('Color:', color.json())
+print('Composition:', composition.json())
+print('Depth:', depth.json())
+print('Palette:', palette.json())
+print('Type:', typee.json())
